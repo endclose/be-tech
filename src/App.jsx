4a55555338bb1ff5968f3d2fd350f348867cd921
Benjamin Bailon <benjamin.bailon@outlook.com>
2023-02-28 18:18:06 -0600

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from "./assets/logo.svg";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const clicked = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="app">
       <img src={reactLogo} alt="Be-Tech Logo" className="logo" />
       <h2>Creating web-based solutions for your business</h2>
+      <h2>
+        {count} <button onClick={clicked}>Click me!</button>
+      </h2>
     </div>
   );
 }
